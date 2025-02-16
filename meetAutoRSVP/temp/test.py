44cbcf0f20ad3e5f2a4450d9f17c8bba14d80557
Ronak Singh <ronaksingh2708@gmail.com>
2025-02-16 11:15:48 +0530

    Events attendance/signup working

--- meetAutoRSVP/temp/test.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-
-driver = webdriver.Firefox(service=Service("/usr/bin/geckodriver"))
-driver.get("https://www.google.com")
-input("Press Enter to close...")
-driver.quit()
